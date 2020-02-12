84a79912e9287339fc9017ba28e89e35c7db0677
Amanda Atkins <amanda@digitalrainstorm.com>
2020-02-12 11:15:53 -0800

    Build out server and API routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,34 @@
+const Book = require("../models/Book.js");
+
+module.exports = app => {
+
+    // Get all saved books
+    app.get("/api/books", (_, res) => {
+        Book.find({})
+        .sort('title')
+        .then(books => res.json(books))
+        .catch(err => console.log(err));
+    }); 
+
+    // Save a book
+    app.post("/api/books", ({ body }, res) => {
+        Book.create(body)
+        .then(newBook => res.json(newBook))
+        .catch(err => res.status(400).json(err));
+    });
+
+    // Get saved book detail
+    app.get("/api/books/:id", ({ params }, res) => {
+        Book.findOne({ _id: params.id})
+        .then(book => res.json(book))
+        .catch(err => res.status(400).json(err));
+    });
+
+    // Delete saved book
+    app.delete("/api/books/:id", ({ params },res) => {
+        Book.deleteOne({ _id: params.id })
+        .then(deletedBook => res.json(deletedBook))
+        .catch(err => res.status(400).json(err));
+    });
+
+}
